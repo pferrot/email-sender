3758e2ca1a816197064ce666121f0473f1c091a7
Patrice Ferrot <patrice.ferrot@protonmail.com>
2010-04-05 16:43:44 +0000

    Initial Development

--- src/main/java/com/pferrot/emailsender/Consts.java
+++ src/main/java/com/pferrot/emailsender/Consts.java
@@ -1,10 +1,5 @@
 package com.pferrot.emailsender;
 
 public interface Consts {
-	
-	String NO_REPLY_ADDRESS = "no_reply@sharedcalendar.com";
-	
-	String DEFAULT_SENDER_NAME = "sharedcalendar.com";
-	String DEFAULT_SENDER_ADDRESS = NO_REPLY_ADDRESS;
 
 }
